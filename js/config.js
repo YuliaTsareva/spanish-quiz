f7cf4e3502847ed795eb172fae896b855b9ed593
YuliaChe <yulia.chebotareva@gmail.com>
2015-09-25 11:38:15 +0300

    Set questions count to 10 per game

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  maxQuestionsCount: 2,
+  maxQuestionsCount: 10,
   optionsCount: 4,
   inputQuestionProbability: 1,
   motivationImage: 'https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg'
