80c3052b5baf4b19b92dd77dfcabef603baf9604
YuliaChe <yulia.chebotareva@gmail.com>
2015-09-25 11:45:24 +0300

    Change input question probability

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 module.exports = {
   maxQuestionsCount: 10,
   optionsCount: 4,
-  inputQuestionProbability: 1,
+  inputQuestionProbability: 0.3,
   motivationImage: 'https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg'
 };
